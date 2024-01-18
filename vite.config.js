cb3e3d5477babbcca153d880495c144294b32d48
Igor <81867285+ITigor1985@users.noreply.github.com>
2024-01-18 11:24:48 +0200

    new vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,32 @@
 import posthtml from "@vituum/vite-plugin-posthtml";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { defineConfig } from "vite";
 
-export default {
-  plugins: [posthtml()],
-  base: "/sferabud/",
-};
+// export default {
+//   plugins: [posthtml()],
+//   base: "/sferabud/",
+// };
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      posthtml(),
+      ViteImageOptimizer({
+        /* pass your config */
+        png: {
+          // https://sharp.pixelplumbing.com/api-output#png
+          quality: 70,
+        },
+        jpeg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 70,
+        },
+        jpg: {
+          // https://sharp.pixelplumbing.com/api-output#jpeg
+          quality: 70,
+        },
+      }),
+    ],
+    base: "/sferabud/",
+  };
+});
